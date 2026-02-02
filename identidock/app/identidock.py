b26848c7498a4bd080a75c8a9d268824e7180c71
Taras <tarasenya@gmail.com>
2026-02-02 18:38:47 +0100

    bla

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -45,4 +45,4 @@ def get_identicon(name):
     return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=9090)
